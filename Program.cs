872b92d0083f49bddb86cd31851acdbb77b1aa7f
NoeDuclos <n.duclos@programisto.fr>
2023-03-07 00:21:48 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using DemoClasse;
+using System.Linq;
+
+List<Enemy> list = Enemy.InitList();
+
+Console.WriteLine("First enemy :" + list.FirstOrDefault().DescribeSelf());
+
+Console.WriteLine("There is " + list.Count() + " Enemy");
+
+Console.WriteLine("The last enemy with more than 2 attack point is called " + list.Where(e => e.Attack > 2).Select(x => x.Name).LastOrDefault());
